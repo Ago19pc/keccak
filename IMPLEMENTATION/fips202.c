184af8260e953fa01acf94bf85bd65f3ca0bb368
ago19 <ago19pc@gmail.com>
2024-06-28 20:59:06 +0200

    init

--- IMPLEMENTATION/fips202.c
+++ IMPLEMENTATION/fips202.c
@@ -118,12 +118,7 @@ static void keccak_squeezeblocks(uint8_t *h, size_t nblocks,
  **************************************************/
 static void keccak_inc_init(uint64_t *s_inc)
 {
-   size_t i;
-
-   for (i = 0; i < 25; ++i) {
-      s_inc[i] = 0;
-   }
-   s_inc[25] = 0;
+   memset(s_inc, 0, sizeof(keccak_state_t));
 }
 /*************************************************
  * Name:        keccak_inc_absorb
