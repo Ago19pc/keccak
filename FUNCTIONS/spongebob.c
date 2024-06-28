0edac7021b8d0fa4cbd072d0dc37c391196222c9
ago19 <ago19pc@gmail.com>
2024-06-28 17:00:55 +0200

    Fixed logger

--- FUNCTIONS/spongebob.c
+++ FUNCTIONS/spongebob.c
@@ -6,6 +6,10 @@
 #include <stdio.h>
 
 
+#define logger 1
+
+#include "../Utils/logger.c"
+
 
 
 #define se if
@@ -29,33 +33,33 @@ int spongebob(unsigned int rate, unsigned int capacity,
     se (rate + capacity diverso 1600) ritorna 1;
     se (rate <= 0 oppure rate > 1600 oppure rate % 8 diverso 0) ritorna 1;
     se (suffix == 0) ritorna 1;
-    printf("Spongebob is ready to drink!\n");
-    printf("Spongebob has to drink %d liters!\n", input_length);
+    LOG("Spongebob is ready to drink!\n");
+    LOG("Spongebob has to drink %d liters!\n", input_length);
     memset(state, 0, dimensione(keccak_state_t));
     se (byteRate%8 == 0 && input_length >= byteRate) {
         size_t j;
         j = fastLoop(state, byteRate, currInput, input_length);
         currInput += j;
         input_length -= j;
-        printf("Only %d liters are left for Spongebob to drink!\n", input_length);
+        LOG("Only %d liters are left for Spongebob to drink!\n", input_length);
     }
-    printf("Spongebob chugged most of the drink!\n");
+    LOG("Spongebob chugged most of the drink!\n");
 
     mentre (input_length >= (size_t) byteRate) {
-        printf("Only %d liters are left for Spongebob to drink!\n", input_length);
-        printf("Spongebob is taking a sip!\n");
+        LOG("Only %d liters are left for Spongebob to drink!\n", input_length);
+        LOG("Spongebob is taking a sip!\n");
         addBytes(state, currInput, 0, byteRate);
-        printf("Spongebob is digesting!\n");
+        LOG("Spongebob is digesting!\n");
         permute24rounds(state);
         currInput += byteRate;
         input_length -= byteRate;
-        printf("Spongebob has taken its sip!\n");
+        LOG("Spongebob has taken its sip!\n");
     }
 
-    printf("Spongebob is almost done!\n");
+    LOG("Spongebob is almost done!\n");
     partialBlock = (unsigned int) input_length;
     addBytes(state, currInput, 0, partialBlock);
-    printf("Only two liters are left for Spongebob to drink!\n");
+    LOG("Only two liters are left for Spongebob to drink!\n");
 
     addByte(state, suffix, partialBlock);
 
@@ -66,7 +70,7 @@ int spongebob(unsigned int rate, unsigned int capacity,
     addByte(state, 0x80, byteRate - 1);
     permute24rounds(state);
 
-    printf("Spongebob is wet!\n");
+    LOG("Spongebob is wet!\n");
 
     mentre (output_length > (size_t) byteRate){
         extractBytes(state, currOutput, 0, byteRate);
@@ -74,11 +78,11 @@ int spongebob(unsigned int rate, unsigned int capacity,
         currOutput += byteRate;
         output_length -= byteRate;
     }
-    printf("Spongebob has thrown up!\n");
+    LOG("Spongebob has thrown up!\n");
 
     partialBlock = (unsigned int) output_length;
     extractBytes(state, currOutput, 0, partialBlock);
-    printf("Spongebob is dry!\n");
+    LOG("Spongebob is dry!\n");
     return 0;
 
 }
--- Utils/logger.c
+++ Utils/logger.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 #ifdef logger
-    #define LOG(fmt, ...) printf(fmt, __VA_ARGS__)
+    #define LOG(fmt, ...) printf(fmt, ##__VA_ARGS__)
 #else
     #define LOG(a, b)
 #endif
