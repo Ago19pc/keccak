27c2d91e6594db218071738b8cc019eea6b41b5e
fillododi <fillo.dodi@gmail.com>
2024-07-01 18:26:00 +0200

    finished avx2 no fastloop (maybe see asm)

--- IMPLEMENTATION/fips202.c
+++ IMPLEMENTATION/fips202.c
@@ -3,77 +3,50 @@
 #include <string.h>
 #include "fips202.h"
 #include "keccakf1600.c"
-static void keccak_absorb(keccak_state_t *s,
-                          uint32_t r,
-                          const uint8_t *m, size_t mlen,
-                          uint8_t p)
-{
-   while (mlen >= r) {
-      KeccakF1600_StateXORBytes(s, m, 0, r);
-      KeccakF1600_StatePermute(s);
-      mlen -= r;
-      m += r;
-   }
 
-   if(mlen > 0) {
-      KeccakF1600_StateXORBytes(s, m, 0, mlen);
-   }
-
-   if(mlen == r-1) {
-      p |= 128;
-      KeccakF1600_StateXORBytes(s, &p, mlen, 1);
-   } else {
-      KeccakF1600_StateXORBytes(s, &p, mlen, 1);
-      p = 128;
-      KeccakF1600_StateXORBytes(s, &p, r-1, 1);
-   }
-}
-
-
-static void keccak_inc_init(keccak_state_t *state)
+static void keccak_init(keccak_state_t *state)
 {
    memset(state, 0, sizeof(keccak_state_t));
 }
 
-static size_t keccak_inc_absorb(keccak_state_t *state, uint32_t r, const uint8_t *m,
-                              size_t mlen)
+static size_t keccak_absorb(keccak_state_t *state, uint32_t rate, const uint8_t *input,
+                              size_t inlen)
 {
-   while (mlen >= r) {
-      KeccakF1600_StateXORBytes(state, m, 0, r);
-      mlen -= (size_t)(r);
-      m += r;
+   while (inlen >= rate) {
+      KeccakF1600_StateXORBytes(state, input, 0, rate);
+      inlen -= (size_t)(rate);
+      input += rate;
       KeccakF1600_StatePermute(state);
    }
-   KeccakF1600_StateXORBytes(state, m, 0, mlen);
-   return mlen;
+   KeccakF1600_StateXORBytes(state, input, 0, inlen);
+   return inlen;
 }
 
-static void keccak_inc_finalize(keccak_state_t *state, uint32_t r, uint8_t p, size_t remainingLen)
+static void keccak_finalize(keccak_state_t *state, uint32_t rate, uint8_t padding, size_t remainingLen)
 {
-   if(remainingLen == r-1) {
-      
-      p |= 128;
-      KeccakF1600_StateXORBytes(state, &p, remainingLen, 1);
+   if(remainingLen == rate-1) {
+      padding |= 128;
+      KeccakF1600_StateXORBytes(state, &padding, remainingLen, 1);
    } else {
-      KeccakF1600_StateXORBytes(state, &p, remainingLen, 1);
-      p = 128;
-      KeccakF1600_StateXORBytes(state, &p, r-1, 1);
+      KeccakF1600_StateXORBytes(state, &padding, remainingLen, 1);
+      padding = 128;
+      KeccakF1600_StateXORBytes(state, &padding, rate-1, 1);
    }
 }
 
-static void keccak_inc_squeeze(uint8_t *h, size_t outlen,
-                               keccak_state_t *state, uint32_t r)
+static void keccak_squeeze(uint8_t *out, size_t outlen,
+                               keccak_state_t *state, uint32_t rate)
 {
    size_t len = 0;
    while (outlen > 0) {
        KeccakF1600_StatePermute(state);
-       if (outlen < r) {
+       if (outlen < rate) {
            len = outlen;
        } else {
-           len = r;
+           len = rate;
        }
-       KeccakF1600_StateExtractBytes(state, h, 0, len);
-       h += len;
+       KeccakF1600_StateExtractBytes(state, out, 0, len);
+       out += len;
        outlen -= len;
    }
 }
@@ -82,12 +55,12 @@ void shake128(uint8_t *output, size_t outlen, const uint8_t *input,
               size_t inlen)
 {
    ALIGN (32) keccak_state_t state;
-   keccak_inc_init(&state);
+   keccak_init(&state);
 
-   inlen = keccak_inc_absorb(&state, SHAKE128_RATE, input, inlen);
-   keccak_inc_finalize(&state, SHAKE128_RATE, 0x1F, inlen);
+   inlen = keccak_absorb(&state, SHAKE128_RATE, input, inlen);
+   keccak_finalize(&state, SHAKE128_RATE, 0x1F, inlen);
 
-   keccak_inc_squeeze(output, outlen, &state, SHAKE128_RATE);
+   keccak_squeeze(output, outlen, &state, SHAKE128_RATE);
 }
 
 void shake256(uint8_t *output, size_t outlen,
@@ -95,37 +68,37 @@ void shake256(uint8_t *output, size_t outlen,
 {
    ALIGN(32) keccak_state_t state;
 
-   keccak_inc_init(&state);
+   keccak_init(&state);
 
-   inlen = keccak_inc_absorb(&state, SHAKE256_RATE, input, inlen);
-   keccak_inc_finalize(&state, SHAKE256_RATE, 0x1F, inlen);
+   inlen = keccak_absorb(&state, SHAKE256_RATE, input, inlen);
+   keccak_finalize(&state, SHAKE256_RATE, 0x1F, inlen);
 
-   keccak_inc_squeeze(output, outlen, &state, SHAKE256_RATE);
+   keccak_squeeze(output, outlen, &state, SHAKE256_RATE);
 }
 void sha3_256(uint8_t *output, const uint8_t *input, size_t inlen)
 {
    ALIGN(32) keccak_state_t state;
-   keccak_inc_init(&state);
-   inlen = keccak_inc_absorb(&state, SHA3_256_RATE, input, inlen);
-   keccak_inc_finalize(&state, SHA3_256_RATE, 0x06, inlen);
-   keccak_inc_squeeze(output, 32, &state, SHA3_256_RATE);
+   keccak_init(&state);
+   inlen = keccak_absorb(&state, SHA3_256_RATE, input, inlen);
+   keccak_finalize(&state, SHA3_256_RATE, 0x06, inlen);
+   keccak_squeeze(output, 32, &state, SHA3_256_RATE);
 }
 void sha3_384(uint8_t *output, const uint8_t *input, size_t inlen)
 {
    ALIGN(32) keccak_state_t state;
-   keccak_inc_init(&state);
+   keccak_init(&state);
 
-   inlen = keccak_inc_absorb(&state, SHA3_384_RATE, input, inlen);
-   keccak_inc_finalize(&state, SHA3_384_RATE, 0x06, inlen);
+   inlen = keccak_absorb(&state, SHA3_384_RATE, input, inlen);
+   keccak_finalize(&state, SHA3_384_RATE, 0x06, inlen);
 
-   keccak_inc_squeeze(output, 48, &state, SHA3_384_RATE);
+   keccak_squeeze(output, 48, &state, SHA3_384_RATE);
 }
 
 void sha3_512(uint8_t *output, const uint8_t *input, size_t inlen)
 {
    ALIGN(32) keccak_state_t state;
-   keccak_inc_init(&state);
-   inlen = keccak_inc_absorb(&state, SHA3_512_RATE, input, inlen);
-   keccak_inc_finalize(&state, SHA3_512_RATE, 0x06, inlen);
-   keccak_inc_squeeze(output, 64, &state, SHA3_512_RATE);
+   keccak_init(&state);
+   inlen = keccak_absorb(&state, SHA3_512_RATE, input, inlen);
+   keccak_finalize(&state, SHA3_512_RATE, 0x06, inlen);
+   keccak_squeeze(output, 64, &state, SHA3_512_RATE);
 }
--- IMPLEMENTATION/keccakf1600.c
+++ IMPLEMENTATION/keccakf1600.c
@@ -18,130 +18,108 @@
 #endif
 
 
-// al contrario perché l'assembly inizia dall'ultima keccak_rc e decrementa il contatore delle rc, quindi la prima deve essere keccak_rc[23] e l'ultima keccak_rc[0]
-typedef uint64_t keccak_rc_t[4];
-ALIGN(32) keccak_rc_t keccak_rc[24] =
-{
-    {0x8000000080008008ull},    //round 23
-    {0x0000000080000001ull},
-    {0x8000000000008080ull},
-    {0x8000000080008081ull},
-    {0x800000008000000Aull},
-    {0x000000000000800Aull},
-    {0x8000000000000080ull},
-    {0x8000000000008002ull},
-    {0x8000000000008003ull},
-    {0x8000000000008089ull},
-    {0x800000000000008Bull},
-    {0x000000008000808Bull},
-    {0x000000008000000Aull},
-    {0x0000000080008009ull},
-    {0x0000000000000088ull},
-    {0x000000000000008Aull},
-    {0x8000000000008009ull},
-    {0x8000000080008081ull},
-    {0x0000000080000001ull},
-    {0x000000000000808Bull},
-    {0x8000000080008000ull},
-    {0x800000000000808Aull},
-    {0x0000000000008082ull},
-    {0x0000000000000001ull},    //round 0
-};
-
-void KeccakF1600_StateExtractBytes(keccak_state_t *s, unsigned char *data,
+void KeccakF1600_StateExtractBytes(keccak_state_t *state, unsigned char *data,
                                    unsigned int offset, unsigned int length)
 {
-    uint64_t  t[25] = {0};
-    uint64_t  *d = (!offset && (length >= sizeof(t))) ? (uint64_t *)data : t;
-    uint64_t  *c4 = (uint64_t *)&s->c4;
-    if ((d == t) && (length > sizeof(t)))
-        length = sizeof(t);
-    _mm256_storeu_si256((__m256i *)(d + 0*5), s->a0);
-    _mm256_storeu_si256((__m256i *)(d + 1*5), s->a1);
-    _mm256_storeu_si256((__m256i *)(d + 2*5), s->a2);
-    _mm256_storeu_si256((__m256i *)(d + 3*5), s->a3);
-    _mm256_storeu_si256((__m256i *)(d + 4*5), s->a4);
-    d[0*5 + 4] = c4[0];
-    d[1*5 + 4] = c4[1];
-    d[2*5 + 4] = c4[2];
-    d[3*5 + 4] = c4[3];
-    d[4*5 + 4] = c4[4];
+    uint64_t  zero[25] = {0};
+    uint64_t  *extractedData;
+    if(length == 200 && !offset){ //chiamata dalla XOR
+        extractedData = data;
+    } else { //chiamata dalla Squeeze
+        extractedData = zero;
+    }
+    //Estrazione righe (tranne ultima colonna)
+    _mm256_storeu_si256((__m256i *)(extractedData + 0*5), state->a0);
+    _mm256_storeu_si256((__m256i *)(extractedData + 1*5), state->a1);
+    _mm256_storeu_si256((__m256i *)(extractedData + 2*5), state->a2);
+    _mm256_storeu_si256((__m256i *)(extractedData + 3*5), state->a3);
+    _mm256_storeu_si256((__m256i *)(extractedData + 4*5), state->a4);
+    //Estrazione ultima colonna.
+    uint64_t *lastCol = (uint64_t *)&state->c4;
+    extractedData[0*5 + 4] = lastCol[0];
+    extractedData[1*5 + 4] = lastCol[1];
+    extractedData[2*5 + 4] = lastCol[2];
+    extractedData[3*5 + 4] = lastCol[3];
+    extractedData[4*5 + 4] = lastCol[4];
 
-    if (d == t)
-        memcpy(data, (uint8_t *)t + offset, length);
+    if (extractedData == zero) //modifico data solo se sto venendo chiamato dalla squeeze
+        memcpy(data, (uint8_t *)zero + offset, length);
 }
 
-void KeccakF1600_StateXORBytes(keccak_state_t *s, const unsigned char *data,
+void KeccakF1600_StateXORBytes(keccak_state_t *state, const unsigned char *data,
                                unsigned int offset, unsigned int length)
 {
-   uint64_t      *d = (uint64_t *)data;
-   uint8_t       *t1, *d1;
-   uint64_t      t[25];
-   uint64_t      *t0;
-   ptrdiff_t   lane_n = length / sizeof(uint64_t);
-   ptrdiff_t   byte_n = length % sizeof(uint64_t);
-   ptrdiff_t   i;
-   KeccakF1600_StateExtractBytes(s, (uint8_t *)t, 0, sizeof(t));
+   uint64_t*     dataWords = (uint64_t *)data;
+   uint64_t      stateWords[25];
+   uint64_t*     firstStateWord = (uint64_t *)((uint8_t *)stateWords + offset);
 
-   t0 = (uint64_t *)((uint8_t *)t + offset);
-   for (i = 0; i < lane_n; i++) {
-      t0[i] ^= d[i];
+   KeccakF1600_StateExtractBytes(state, (uint8_t *)stateWords, 0, 200);
+   for (int i = 0; i < length/8; i++) {
+      *(firstStateWord + i) ^= dataWords[i];
    }
-
-   if (byte_n) {
-      t1 = (uint8_t *)(t0 + i);
-      d1 = (uint8_t *)(d + i);
-
-      for (i = 0; i < byte_n; i++) t1[i] ^= d1[i];
+   if (length % 8) {
+      uint8_t* firstRemainingStateByte = (uint8_t *)(firstStateWord + length/8);
+      uint8_t* firstRemainingDataByte = (uint8_t *)(dataWords + length/8);
+      for (int i = 0; i < length%8; i++) *(firstRemainingStateByte + i) ^= *(firstRemainingDataByte + i);
    }
-   s->a0 = _mm256_loadu_si256((const __m256i *)(t + 0*5));
-   s->a1 = _mm256_loadu_si256((const __m256i *)(t + 1*5));
-   s->a2 = _mm256_loadu_si256((const __m256i *)(t + 2*5));
-   s->a3 = _mm256_loadu_si256((const __m256i *)(t + 3*5));
-   s->a4 = _mm256_loadu_si256((const __m256i *)(t + 4*5));
-   s->c4 = _mm256_setr_epi64x(t[0*5 + 4], t[1*5 + 4], t[2*5 + 4], t[3*5 + 4]);
-   s->a44 = _mm256_set1_epi64x(t[4*5 + 4]);
+   state->a0 = _mm256_loadu_si256((const __m256i *)(stateWords));
+   state->a1 = _mm256_loadu_si256((const __m256i *)(stateWords + 5));
+   state->a2 = _mm256_loadu_si256((const __m256i *)(stateWords + 10));
+   state->a3 = _mm256_loadu_si256((const __m256i *)(stateWords + 15));
+   state->a4 = _mm256_loadu_si256((const __m256i *)(stateWords + 20));
+   state->c4 = _mm256_setr_epi64x(stateWords[4], stateWords[9], stateWords[14], stateWords[19]);
+   state->a44 = _mm256_set1_epi64x(stateWords[24]);
 }
 
-void KeccakF1600_StatePermute(keccak_state_t *s)
+void KeccakF1600_StatePermute(keccak_state_t *state)
 {
-    __m256i SLLVA0 = _mm256_setr_epi64x( 0,  1, 62, 28);
-    __m256i SRLVA0 = _mm256_setr_epi64x(64, 63, 2, 36);
-    __m256i SLLVA1 = _mm256_setr_epi64x(36, 44,  6, 55);
-    __m256i SRLVA1 = _mm256_setr_epi64x(28, 20, 58, 9);
-    __m256i SLLVA2 = _mm256_setr_epi64x( 3, 10, 43, 25);
-    __m256i SRLVA2 = _mm256_setr_epi64x(61, 54, 21, 39);
-    __m256i SLLVA3 = _mm256_setr_epi64x(41, 45, 15, 21);
-    __m256i SRLVA3 = _mm256_setr_epi64x(23, 19, 49, 43);
-    __m256i SLLVA4 = _mm256_setr_epi64x(18, 2, 61, 56);
-    __m256i SRLVA4 = _mm256_setr_epi64x(46, 62, 3, 8);
-    __m256i SLLVC4 = _mm256_setr_epi64x(27, 20, 39,  8);
-    __m256i SRLVC4 = _mm256_setr_epi64x(37, 44, 25, 56);
-    volatile __m256i SLLV_A0 = _mm256_setr_epi64x( 0,  1, 62, 28);
-    volatile __m256i SRLV_A0 = _mm256_setr_epi64x(64, 63, 2, 36);
-    volatile __m256i SLLV_A1 = _mm256_setr_epi64x(36, 44,  6, 55);
-    volatile __m256i SRLV_A1 = _mm256_setr_epi64x(28, 20, 58, 9);
-    volatile __m256i SLLV_A2 = _mm256_setr_epi64x( 3, 10, 43, 25);
-    volatile __m256i SRLV_A2 = _mm256_setr_epi64x(61, 54, 21, 39);
-    volatile __m256i SLLV_A3 = _mm256_setr_epi64x(41, 45, 15, 21);
-    volatile __m256i SRLV_A3 = _mm256_setr_epi64x(23, 19, 49, 43);
-    volatile __m256i SLLV_A4 = _mm256_setr_epi64x(18, 2, 61, 56);
-    volatile __m256i SRLV_A4 = _mm256_setr_epi64x(46, 62, 3, 8);
-    volatile __m256i SLLV_C4 = _mm256_setr_epi64x(27, 20, 39,  8);
-    volatile __m256i SRLV_C4 = _mm256_setr_epi64x(37, 44, 25, 56);
-    __m256i a0, a1, a2, a3, a4, c4;
-    __m256i a04, a14, a24, a34, a44;
-    __m256i b0, b1, b2, b3, b4;
-    __m256i b04, b14, b24, b34, b44;
-    __m256i r0, r1, r2, r3;
-    ptrdiff_t       round_i;
-    a0 = _mm256_loadu_si256(&s->a0);
-    a1 = _mm256_loadu_si256(&s->a1);
-    a2 = _mm256_loadu_si256(&s->a2);
-    a3 = _mm256_loadu_si256(&s->a3);
-    a4 = _mm256_loadu_si256(&s->a4);
-    c4 = _mm256_loadu_si256(&s->c4);
-    a44 = _mm256_loadu_si256(&s->a44);
+    // al contrario perché l'assembly inizia dall'ultima keccak_rc e decrementa il contatore delle rc, quindi la prima deve essere keccak_rc[23] e l'ultima keccak_rc[0]
+    ALIGN(32) __m256i round_constants[24] = {
+            {0x8000000080008008ull},    //round 23
+            {0x0000000080000001ull},
+            {0x8000000000008080ull},
+            {0x8000000080008081ull},
+            {0x800000008000000Aull},
+            {0x000000000000800Aull},
+            {0x8000000000000080ull},
+            {0x8000000000008002ull},
+            {0x8000000000008003ull},
+            {0x8000000000008089ull},
+            {0x800000000000008Bull},
+            {0x000000008000808Bull},
+            {0x000000008000000Aull},
+            {0x0000000080008009ull},
+            {0x0000000000000088ull},
+            {0x000000000000008Aull},
+            {0x8000000000008009ull},
+            {0x8000000080008081ull},
+            {0x0000000080000001ull},
+            {0x000000000000808Bull},
+            {0x8000000080008000ull},
+            {0x800000000000808Aull},
+            {0x0000000000008082ull},
+            {0x0000000000000001ull},    //round 0
+    };
+    volatile __m256i left_rotation_constant_a0 = _mm256_setr_epi64x( 0,  1, 62, 28);
+    volatile __m256i right_rotation_constant_a0 = _mm256_setr_epi64x(64, 63, 2, 36);
+    volatile __m256i left_rotation_constant_a1 = _mm256_setr_epi64x(36, 44,  6, 55);
+    volatile __m256i right_rotation_constant_a1 = _mm256_setr_epi64x(28, 20, 58, 9);
+    volatile __m256i left_rotation_constant_a2 = _mm256_setr_epi64x( 3, 10, 43, 25);
+    volatile __m256i right_rotation_constant_a2 = _mm256_setr_epi64x(61, 54, 21, 39);
+    volatile __m256i left_rotation_constant_a3 = _mm256_setr_epi64x(41, 45, 15, 21);
+    volatile __m256i right_rotation_constant_a3 = _mm256_setr_epi64x(23, 19, 49, 43);
+    volatile __m256i left_rotation_constant_a4 = _mm256_setr_epi64x(18, 2, 61, 56);
+    volatile __m256i right_rotation_constant_a4 = _mm256_setr_epi64x(46, 62, 3, 8);
+    volatile __m256i left_rotation_constant_c4 = _mm256_setr_epi64x(27, 20, 39,  8);
+    volatile __m256i right_rotation_constant_c4 = _mm256_setr_epi64x(37, 44, 25, 56);
+    __m256i a0, a1, a2, a3, a4, c4, a44;
+    a0 = _mm256_loadu_si256(&state->a0);
+    a1 = _mm256_loadu_si256(&state->a1);
+    a2 = _mm256_loadu_si256(&state->a2);
+    a3 = _mm256_loadu_si256(&state->a3);
+    a4 = _mm256_loadu_si256(&state->a4);
+    c4 = _mm256_loadu_si256(&state->c4);
+    a44 = _mm256_loadu_si256(&state->a44);
     __asm volatile(
         "movq           %7, %%rax\n"
     "1:\n"
@@ -174,17 +152,17 @@ void KeccakF1600_StatePermute(keccak_state_t *s)
         "vpxor          %%ymm0, %2, %2\n"
         "vpxor          %%ymm0, %3, %3\n"
         "vpxor          %%ymm0, %4, %4\n"
-        "vmovdqa        %[SRLV_A0], %%ymm6\n"
-        "vmovdqa        %[SLLV_A0], %6\n"
-        "vmovdqa        %[SRLV_A1], %%ymm7\n"
+        "vmovdqa        %[right_rotation_constant_a0], %%ymm6\n"
+        "vmovdqa        %[left_rotation_constant_a0], %6\n"
+        "vmovdqa        %[right_rotation_constant_a1], %%ymm7\n"
         "vpsrlvq        %%ymm6, %0, %%ymm6\n"
         "vpsllvq        %6, %0, %0\n"
-        "vmovdqa        %[SLLV_A1], %6\n"
-        "vmovdqa        %[SRLV_A2], %%ymm0\n"
+        "vmovdqa        %[left_rotation_constant_a1], %6\n"
+        "vmovdqa        %[right_rotation_constant_a2], %%ymm0\n"
         "vpsrlvq        %%ymm7, %1, %%ymm7\n"
         "vpsllvq        %6, %1, %1\n"
-        "vmovdqa        %[SLLV_A2], %6\n"
-        "vmovdqa        %[SRLV_A3], %%ymm10\n"
+        "vmovdqa        %[left_rotation_constant_a2], %6\n"
+        "vmovdqa        %[right_rotation_constant_a3], %%ymm10\n"
         "vpsrlvq        %%ymm0, %2, %%ymm0\n"
         "vpsllvq        %6, %2, %2\n"
         "vpxor          %%ymm7, %1, %%ymm7\n"
@@ -194,20 +172,20 @@ void KeccakF1600_StatePermute(keccak_state_t *s)
         "vpxor          %%ymm0, %2, %2\n"
         "vpsrlvq        %%ymm10, %3, %%ymm0\n"
         "vpermq         $114, %2, %2\n"
-        "vmovdqa        %[SLLV_A3], %%ymm10\n"
+        "vmovdqa        %[left_rotation_constant_a3], %%ymm10\n"
         "vpsllvq        %%ymm10, %3, %3\n"
         "vpxor          %%ymm0, %3, %%ymm10\n"
         "vpermq         $135, %%ymm10, %3\n"
-        "vmovdqa        %[SRLV_A4], %%ymm0\n"
+        "vmovdqa        %[right_rotation_constant_a4], %%ymm0\n"
         "vpsrlvq        %%ymm0, %4, %6\n"
-        "vmovdqa        %[SLLV_A4], %%ymm0\n"
+        "vmovdqa        %[left_rotation_constant_a4], %%ymm0\n"
         "vpsllvq        %%ymm0, %4, %4\n"
         "vpxor          %6, %4, %%ymm0\n"
         "vpermq         $201, %%ymm0, %0\n"
         "vpunpckhqdq    %%ymm0, %%ymm0, %%ymm0\n"
-        "vmovdqa        %[SRLV_C4], %4\n"
+        "vmovdqa        %[right_rotation_constant_c4], %4\n"
         "vpsrlvq        %4, %5, %6\n"
-        "vmovdqa        %[SLLV_C4], %4\n"
+        "vmovdqa        %[left_rotation_constant_c4], %4\n"
         "vpsllvq        %4, %5, %5\n"
         "vpxor          %6, %5, %4\n"
         "vpsrlq         $50, %%ymm9, %5\n"
@@ -260,21 +238,21 @@ void KeccakF1600_StatePermute(keccak_state_t *s)
         "vpxor          %6, %%ymm0, %6\n"
         "jnz            1b\n"
         : "+x"(a0), "+x"(a1), "+x"(a2), "+x"(a3), "+x"(a4), "+x"(c4), "+x"(a44)
-        : "i"(8*4*24), "d"(keccak_rc),
-            [SLLV_A0] "m"(SLLV_A0), [SRLV_A0] "m"(SRLV_A0),
-            [SLLV_A1] "m"(SLLV_A1), [SRLV_A1] "m"(SRLV_A1),
-            [SLLV_A2] "m"(SLLV_A2), [SRLV_A2] "m"(SRLV_A2),
-            [SLLV_A3] "m"(SLLV_A3), [SRLV_A3] "m"(SRLV_A3),
-            [SLLV_A4] "m"(SLLV_A4), [SRLV_A4] "m"(SRLV_A4),
-            [SLLV_C4] "m"(SLLV_C4), [SRLV_C4] "m"(SRLV_C4)
+        : "i"(768), "d"(round_constants),
+            [left_rotation_constant_a0] "m"(left_rotation_constant_a0), [right_rotation_constant_a0] "m"(right_rotation_constant_a0),
+            [left_rotation_constant_a1] "m"(left_rotation_constant_a1), [right_rotation_constant_a1] "m"(right_rotation_constant_a1),
+            [left_rotation_constant_a2] "m"(left_rotation_constant_a2), [right_rotation_constant_a2] "m"(right_rotation_constant_a2),
+            [left_rotation_constant_a3] "m"(left_rotation_constant_a3), [right_rotation_constant_a3] "m"(right_rotation_constant_a3),
+            [left_rotation_constant_a4] "m"(left_rotation_constant_a4), [right_rotation_constant_a4] "m"(right_rotation_constant_a4),
+            [left_rotation_constant_c4] "m"(left_rotation_constant_c4), [right_rotation_constant_c4] "m"(right_rotation_constant_c4)
         : "rax", "xmm0", "xmm6", "xmm7", "xmm8", "xmm9", "xmm10"
     );
 
-    _mm256_storeu_si256(&s->a0, a0);
-    _mm256_storeu_si256(&s->a1, a1);
-    _mm256_storeu_si256(&s->a2, a2);
-    _mm256_storeu_si256(&s->a3, a3);
-    _mm256_storeu_si256(&s->a4, a4);
-    _mm256_storeu_si256(&s->c4, c4);
-    _mm256_storeu_si256(&s->a44, a44);
+    _mm256_storeu_si256(&state->a0, a0);
+    _mm256_storeu_si256(&state->a1, a1);
+    _mm256_storeu_si256(&state->a2, a2);
+    _mm256_storeu_si256(&state->a3, a3);
+    _mm256_storeu_si256(&state->a4, a4);
+    _mm256_storeu_si256(&state->c4, c4);
+    _mm256_storeu_si256(&state->a44, a44);
 }
