66d942ba5eb00100d420f8268f4f06b8e37db706
fillododi <fillo.dodi@gmail.com>
2024-07-06 17:51:18 +0200

    ora arm usa permute cross

--- ARM_IMPL/keccakf1600.c
+++ ARM_IMPL/keccakf1600.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <arm_neon.h>
 
+#define ROL(a, offset) ((a << offset) ^ (a >> (64-offset)))
 
 #ifdef ALIGN
 #undef ALIGN
@@ -95,7 +96,7 @@ void KeccakF1600_StateXORBytes(keccak_state_t *state, const unsigned char *data,
     state->a24 = stateWords[24];
 }
 
-void KeccakF1600_StatePermute(keccak_state_t * state){
+void KeccakF1600_StatePermute(uint64_t * state){
     ALIGN(32) uint64_t round_constants[24] = {
             0x0000000000000001,
             0x0000000000008082,
@@ -122,173 +123,264 @@ void KeccakF1600_StatePermute(keccak_state_t * state){
             0x0000000080000001,
             0x8000000080008008
     };
-    uint64_t *currentStateWord = (uint64_t *)state;
-    uint64x2x4_t s0 = vld1q_u64_x4(currentStateWord);
-    uint64x2x4_t s1 = vld1q_u64_x4(currentStateWord + 8);
-    uint64x2x4_t s2 = vld1q_u64_x4(currentStateWord + 16);
-    uint64x2_t s3 = vld1q_u64(currentStateWord + 24); //forse segfault perché s3 è un solo elemento?
+    int round;
 
-    for (uint64_t i = 0; i < 24; i++) {
-        //theta
-        uint64x2_t v0 = veorq_u64(((uint64x2_t*)(&s0))[0], ((uint64x2_t*)(&s1))[2]);
-        uint64x2_t v1 = veorq_u64(((uint64x2_t*)(&s0))[1], ((uint64x2_t*)(&s1))[3]);
-        uint64x2_t v2 = veorq_u64(((uint64x2_t*)(&s0))[2], ((uint64x2_t*)(&s2))[1]);
-        uint64x2_t v3 = veorq_u64(((uint64x2_t*)(&s0))[3], ((uint64x2_t*)(&s1))[0]);
-        uint64x2_t v4 = veorq_u64(((uint64x2_t*)(&s1))[1], ((uint64x2_t*)(&s2))[3]);
-        v1 = veorq_u64(v1, ((uint64x2_t*)(&s2))[0]);
-        v3 = veorq_u64(v3, ((uint64x2_t*)(&s2))[2]);
-        uint64x2_t v5 = vtrn1q_u64(v0, v1);
-        uint64x2_t v6 = vtrn2q_u64(v1, v2);
-        v1 = veorq_u64(v5, v6);
-        v5 = vextq_u64(v4, v2, 1);
-        v3 = veorq_u64(v3, v5);
-        ((uint64_t*)(&v5))[0] = ((uint64_t*)(&v0))[1];
-        v4 = veorq_u64(v4, v5);
-        v4 = veorq_u64(v4, s3);
-        v2 = vextq_u64(v1, v1, 1);
-        ((uint64_t*)(&v4))[1] = ((uint64_t*)(&v3))[0];
-        v0 = vtrn2q_u64(v3, v1);
-        v5 = vshlq_n_u64(v2, 1);
-        v5 = vsriq_n_u64(v5, v2, 63);
-        v6 = vshlq_n_u64(v3, 1);
-        v6 = vsriq_n_u64(v6, v3, 63);
-        uint64x2_t v7 = vshlq_n_u64(v4, 1);
-        v7 = vsriq_n_u64(v7, v4, 63);
-        uint64x2_t v18 = veorq_u64(v4, v5);
-        v2 = veorq_u64(v2, v6);
-        v0 = veorq_u64(v0, v7);
-        v7 = vextq_u64(v5, v7, 1);
-        v7 = veorq_u64(v3, v7);
-        v6 = vextq_u64(v6, v5, 1);
-        v4 = vtrn1q_u64(v1, v4);
-        v6 = veorq_u64(v4, v6);
-        ((uint64x2_t*)(&s0))[0] = veorq_u64(((uint64x2_t*)(&s0))[0], v18);
-        ((uint64x2_t*)(&s0))[1] = veorq_u64(((uint64x2_t*)(&s0))[1], v6);
-        ((uint64x2_t*)(&s0))[2] = veorq_u64(((uint64x2_t*)(&s0))[2], v2);
-        ((uint64x2_t*)(&s0))[3] = veorq_u64(((uint64x2_t*)(&s0))[3], v0);
-        ((uint64x2_t*)(&s1))[0] = veorq_u64(((uint64x2_t*)(&s1))[0], v0);
-        ((uint64x2_t*)(&s1))[1] = veorq_u64(((uint64x2_t*)(&s1))[1], v7);
-        ((uint64x2_t*)(&s1))[2] = veorq_u64(((uint64x2_t*)(&s1))[2], v18);
-        ((uint64x2_t*)(&s1))[3] = veorq_u64(((uint64x2_t*)(&s1))[3], v6);
-        ((uint64x2_t*)(&s2))[0] = veorq_u64(((uint64x2_t*)(&s2))[0], v6);
-        ((uint64x2_t*)(&s2))[1] = veorq_u64(((uint64x2_t*)(&s2))[1], v2);
-        ((uint64x2_t*)(&s2))[2] = veorq_u64(((uint64x2_t*)(&s2))[2], v0);
-        ((uint64x2_t*)(&s2))[3] = veorq_u64(((uint64x2_t*)(&s2))[3], v7);
-        s3 = veorq_u64(s3, v7);
+    uint64_t Aba, Abe, Abi, Abo, Abu;
+    uint64_t Aga, Age, Agi, Ago, Agu;
+    uint64_t Aka, Ake, Aki, Ako, Aku;
+    uint64_t Ama, Ame, Ami, Amo, Amu;
+    uint64_t Asa, Ase, Asi, Aso, Asu;
+    uint64_t BCa, BCe, BCi, BCo, BCu;
+    uint64_t Da, De, Di, Do, Du;
+    uint64_t Eba, Ebe, Ebi, Ebo, Ebu;
+    uint64_t Ega, Ege, Egi, Ego, Egu;
+    uint64_t Eka, Eke, Eki, Eko, Eku;
+    uint64_t Ema, Eme, Emi, Emo, Emu;
+    uint64_t Esa, Ese, Esi, Eso, Esu;
 
-        
-        
-        
+    //copyFromState(A, state)
+    Aba = state[ 0];
+    Abe = state[ 1];
+    Abi = state[ 2];
+    Abo = state[ 3];
+    Abu = state[ 4];
+    Aga = state[ 5];
+    Age = state[ 6];
+    Agi = state[ 7];
+    Ago = state[ 8];
+    Agu = state[ 9];
+    Aka = state[10];
+    Ake = state[11];
+    Aki = state[12];
+    Ako = state[13];
+    Aku = state[14];
+    Ama = state[15];
+    Ame = state[16];
+    Ami = state[17];
+    Amo = state[18];
+    Amu = state[19];
+    Asa = state[20];
+    Ase = state[21];
+    Asi = state[22];
+    Aso = state[23];
+    Asu = state[24];
 
-        //ro pi
-        uint64_t temp1 = ((uint64_t*)(&s1))[4]; // 1 CORRETTO
-        ((uint64_t*)(&s1))[4] = ((uint64_t*)(&s0))[2] << 1 | ((uint64_t*)(&s0))[2] >> 63;
-	    uint64_t temp2 = ((uint64_t*)(&s0))[7]; // 2
-        ((uint64_t*)(&s0))[7] = temp1 << 3 | temp1 >> 61;
-        temp1 = ((uint64_t*)(&s1))[6]; // 3
-        ((uint64_t*)(&s1))[6] = temp2 << 6 | temp2 >> 58;
-        temp2 = ((uint64_t*)(&s2))[2]; // 4
-        ((uint64_t*)(&s2))[2] = temp1 << 10 | temp1 >> 54;
-        temp1 = ((uint64_t*)(&s2))[4]; // 5
-        ((uint64_t*)(&s2))[4] = temp2 << 15 | temp2 >> 49;
-        temp2 = ((uint64_t*)(&s0))[6]; // 6
-        ((uint64_t*)(&s0))[6] = temp1 << 21 | temp1 >> 43;
-        temp1 = ((uint64_t*)(&s0))[3]; // 7
-        ((uint64_t*)(&s0))[3] = temp2 << 28 | temp2 >> 36;
-        temp2 = ((uint64_t*)(&s2))[0]; // 8
-        ((uint64_t*)(&s2))[0] = temp1 << 36 | temp1 >> 28;
-        temp1 = ((uint64_t*)(&s1))[0]; // 9 <- era qui ora corretto
-        ((uint64_t*)(&s1))[0] = temp2 << 45 | temp2 >> 19;
-        temp2 = ((uint64_t*)(&s2))[3]; // 10
-        ((uint64_t*)(&s2))[3] = temp1 << 55 | temp1 >> 9;
-        temp1 = ((uint64_t*)(&s3))[0]; // 11
-        
+    for( round = 0; round < 24; round += 2 ) {
+        //    prepareTheta
+        BCa = Aba^Aga^Aka^Ama^Asa;
+        BCe = Abe^Age^Ake^Ame^Ase;
+        BCi = Abi^Agi^Aki^Ami^Asi;
+        BCo = Abo^Ago^Ako^Amo^Aso;
+        BCu = Abu^Agu^Aku^Amu^Asu;
 
-	    s3 = vsetq_lane_u64(temp2 << 2 | temp2 >> 62, s3, 0);
-	    vsetq_lane_u64(0, s3, 1);
+        //thetaRhoPiChiIotaPrepareTheta(round  , A, E)
+        Da = BCu^ROL(BCe, 1);
+        De = BCa^ROL(BCi, 1);
+        Di = BCe^ROL(BCo, 1);
+        Do = BCi^ROL(BCu, 1);
+        Du = BCo^ROL(BCa, 1);
 
-        temp2 = ((uint64_t*)(&s0))[1]; // 12
-        ((uint64_t*)(&s0))[1] = temp1 << 14 | temp1 >> 50;
-        temp1 = ((uint64_t*)(&s1))[7]; // 13
-        ((uint64_t*)(&s1))[7] = temp2 << 27 | temp2 >> 37;
-        temp2 = ((uint64_t*)(&s2))[7]; // 14
-        ((uint64_t*)(&s2))[7] = temp1 << 41 | temp1 >> 23;
-        temp1 = ((uint64_t*)(&s2))[6]; // 15
-        ((uint64_t*)(&s2))[6] = temp2 << 56 | temp2 >> 8;
-        temp2 = ((uint64_t*)(&s1))[3]; // 16
-        ((uint64_t*)(&s1))[3] = temp1 << 8 | temp1 >> 56;
-        temp1 = ((uint64_t*)(&s1))[1]; // 17
-        ((uint64_t*)(&s1))[1] = temp2 << 25 | temp2 >> 39;
-        temp2 = ((uint64_t*)(&s0))[4]; // 18
-        ((uint64_t*)(&s0))[4] = temp1 << 43 | temp1 >> 21;
-        temp1 = ((uint64_t*)(&s2))[1]; // 19
-        ((uint64_t*)(&s2))[1] = temp2 << 62 | temp2 >> 2;
-        temp2 = ((uint64_t*)(&s1))[5]; // 20
-        ((uint64_t*)(&s1))[5] = temp1 << 18 | temp1 >> 46;
-        temp1 = ((uint64_t*)(&s2))[5]; // 21
-        ((uint64_t*)(&s2))[5] = temp2 << 39 | temp2 >> 25;
-        temp2 = ((uint64_t*)(&s1))[2]; // 22
-        ((uint64_t*)(&s1))[2] = temp1 << 61 | temp1 >> 3;
-        temp1 = ((uint64_t*)(&s0))[5]; // 23
-        ((uint64_t*)(&s0))[5] = temp2 << 20 | temp2 >> 44;
-        ((uint64_t*)(&s0))[2] = temp1 << 44 | temp1 >> 20;
+        Aba ^= Da;
+        BCa = Aba;
+        Age ^= De;
+        BCe = ROL(Age, 44);
+        Aki ^= Di;
+        BCi = ROL(Aki, 43);
+        Amo ^= Do;
+        BCo = ROL(Amo, 21);
+        Asu ^= Du;
+        BCu = ROL(Asu, 14);
+        Eba =   BCa ^((~BCe)&  BCi );
+        Eba ^= (uint64_t)round_constants[round];
+        Ebe =   BCe ^((~BCi)&  BCo );
+        Ebi =   BCi ^((~BCo)&  BCu );
+        Ebo =   BCo ^((~BCu)&  BCa );
+        Ebu =   BCu ^((~BCa)&  BCe );
 
-        //chi
-        v18 = vextq_u64(((uint64x2_t*)(&s1))[3], s3, 1);
-        v6 = vbicq_u64(((uint64x2_t*)(&s2))[0], v18);
-        uint64x2_t v17 = vextq_u64(((uint64x2_t*)(&s1))[3], s3, 1);
-        v5 = vbicq_u64(v17, ((uint64x2_t*)(&s2))[3]);
-        v3 = vbicq_u64(((uint64x2_t*)(&s2))[3], ((uint64x2_t*)(&s2))[2]);
-        ((uint64x2_t*)(&s2))[3] = veorq_u64(((uint64x2_t*)(&s2))[3], v6);
-        v18 = vtrn1q_u64(((uint64x2_t*)(&s1))[3], ((uint64x2_t*)(&s1))[2]);
-        v17 = vextq_u64(((uint64x2_t*)(&s1))[0], ((uint64x2_t*)(&s1))[3], 1);
-        v7 = vbicq_u64(v17, v18);
-        v18 = vtrn2q_u64(((uint64x2_t*)(&s0))[1], ((uint64x2_t*)(&s1))[2]);
-        v17 = vextq_u64(((uint64x2_t*)(&s0))[2], ((uint64x2_t*)(&s1))[2], 1);
-        v6 = vbicq_u64(v17, v18);
-        v18 = vtrn1q_u64(((uint64x2_t*)(&s0))[1], ((uint64x2_t*)(&s0))[0]);
-        v17 = vtrn1q_u64(((uint64x2_t*)(&s0))[2], ((uint64x2_t*)(&s0))[1]);
-        v1 = vbicq_u64(v17, v18);
-        v18 = vextq_u64(((uint64x2_t*)(&s0))[0], ((uint64x2_t*)(&s1))[0], 1);
-        v17 = vtrn1q_u64(((uint64x2_t*)(&s0))[0], ((uint64x2_t*)(&s1))[1]);
-        v0 = vbicq_u64(v17, v18);
-        v18 = vextq_u64(((uint64x2_t*)(&s1))[0], ((uint64x2_t*)(&s2))[0], 1);
-        v17 = vextq_u64(((uint64x2_t*)(&s1))[1], ((uint64x2_t*)(&s2))[1], 1);
-        v4 = vbicq_u64(v17, v18);
-        ((uint64_t*)(&v18))[0] = ((uint64_t*)(&s2))[1];
-        ((uint64_t*)(&v17))[0] = ((uint64_t*)(&s2))[3];
-        v2 = vbicq_u64(v17, v18);
-        s3 = veorq_u64(s3, v2);
-        v2 = vbicq_u64(((uint64x2_t*)(&s2))[2], ((uint64x2_t*)(&s2))[1]);
-        ((uint64x2_t*)(&s2))[0] = veorq_u64(((uint64x2_t*)(&s2))[0], v2);
-        v2 = vbicq_u64(((uint64x2_t*)(&s0))[3], ((uint64x2_t*)(&s0))[2]);
-        ((uint64x2_t*)(&s2))[1] = veorq_u64(((uint64x2_t*)(&s2))[1], v3);
-        ((uint64x2_t*)(&s2))[2] = veorq_u64(((uint64x2_t*)(&s2))[2], v5);
-        v17 = vextq_u64(((uint64x2_t*)(&s0))[0], ((uint64x2_t*)(&s1))[0], 1);
-        v3 = vbicq_u64(v17, ((uint64x2_t*)(&s0))[3]);
-        v17 = vtrn2q_u64(((uint64x2_t*)(&s0))[1], ((uint64x2_t*)(&s1))[2]);
-        v5 = vbicq_u64(v17, ((uint64x2_t*)(&s1))[1]);
-        ((uint64x2_t*)(&s0))[0] = veorq_u64(((uint64x2_t*)(&s0))[0], v1);
-        ((uint64x2_t*)(&s0))[1] = veorq_u64(((uint64x2_t*)(&s0))[1], v2);
-        ((uint64x2_t*)(&s0))[2] = veorq_u64(((uint64x2_t*)(&s0))[2], v3);
-        ((uint64x2_t*)(&s0))[3] = veorq_u64(((uint64x2_t*)(&s0))[3], v0);
-        ((uint64x2_t*)(&s1))[0] = veorq_u64(((uint64x2_t*)(&s1))[0], v5);
-        ((uint64x2_t*)(&s1))[1] = veorq_u64(((uint64x2_t*)(&s1))[1], v6);
-        ((uint64x2_t*)(&s1))[2] = veorq_u64(((uint64x2_t*)(&s1))[2], v7);
-        ((uint64x2_t*)(&s1))[3] = veorq_u64(((uint64x2_t*)(&s1))[3], v4);
+        Abo ^= Do;
+        BCa = ROL(Abo, 28);
+        Agu ^= Du;
+        BCe = ROL(Agu, 20);
+        Aka ^= Da;
+        BCi = ROL(Aka,  3);
+        Ame ^= De;
+        BCo = ROL(Ame, 45);
+        Asi ^= Di;
+        BCu = ROL(Asi, 61);
+        Ega =   BCa ^((~BCe)&  BCi );
+        Ege =   BCe ^((~BCi)&  BCo );
+        Egi =   BCi ^((~BCo)&  BCu );
+        Ego =   BCo ^((~BCu)&  BCa );
+        Egu =   BCu ^((~BCa)&  BCe );
 
+        Abe ^= De;
+        BCa = ROL(Abe,  1);
+        Agi ^= Di;
+        BCe = ROL(Agi,  6);
+        Ako ^= Do;
+        BCi = ROL(Ako, 25);
+        Amu ^= Du;
+        BCo = ROL(Amu,  8);
+        Asa ^= Da;
+        BCu = ROL(Asa, 18);
+        Eka =   BCa ^((~BCe)&  BCi );
+        Eke =   BCe ^((~BCi)&  BCo );
+        Eki =   BCi ^((~BCo)&  BCu );
+        Eko =   BCo ^((~BCu)&  BCa );
+        Eku =   BCu ^((~BCa)&  BCe );
 
-        //iota
-        uint64x2_t roundConstant = {0, 0};
-        ((uint64_t*)(&roundConstant))[0] = round_constants[i]; //forse segfault perché roundConstant va inizializzato prima?
-        ((uint64x2_t*)(&s0))[0] = veorq_u64(((uint64x2_t*)(&s0))[0], roundConstant);
+        Abu ^= Du;
+        BCa = ROL(Abu, 27);
+        Aga ^= Da;
+        BCe = ROL(Aga, 36);
+        Ake ^= De;
+        BCi = ROL(Ake, 10);
+        Ami ^= Di;
+        BCo = ROL(Ami, 15);
+        Aso ^= Do;
+        BCu = ROL(Aso, 56);
+        Ema =   BCa ^((~BCe)&  BCi );
+        Eme =   BCe ^((~BCi)&  BCo );
+        Emi =   BCi ^((~BCo)&  BCu );
+        Emo =   BCo ^((~BCu)&  BCa );
+        Emu =   BCu ^((~BCa)&  BCe );
 
+        Abi ^= Di;
+        BCa = ROL(Abi, 62);
+        Ago ^= Do;
+        BCe = ROL(Ago, 55);
+        Aku ^= Du;
+        BCi = ROL(Aku, 39);
+        Ama ^= Da;
+        BCo = ROL(Ama, 41);
+        Ase ^= De;
+        BCu = ROL(Ase,  2);
+        Esa =   BCa ^((~BCe)&  BCi );
+        Ese =   BCe ^((~BCi)&  BCo );
+        Esi =   BCi ^((~BCo)&  BCu );
+        Eso =   BCo ^((~BCu)&  BCa );
+        Esu =   BCu ^((~BCa)&  BCe );
 
+        //    prepareTheta
+        BCa = Eba^Ega^Eka^Ema^Esa;
+        BCe = Ebe^Ege^Eke^Eme^Ese;
+        BCi = Ebi^Egi^Eki^Emi^Esi;
+        BCo = Ebo^Ego^Eko^Emo^Eso;
+        BCu = Ebu^Egu^Eku^Emu^Esu;
 
+        //thetaRhoPiChiIotaPrepareTheta(round+1, E, A)
+        Da = BCu^ROL(BCe, 1);
+        De = BCa^ROL(BCi, 1);
+        Di = BCe^ROL(BCo, 1);
+        Do = BCi^ROL(BCu, 1);
+        Du = BCo^ROL(BCa, 1);
+
+        Eba ^= Da;
+        BCa = Eba;
+        Ege ^= De;
+        BCe = ROL(Ege, 44);
+        Eki ^= Di;
+        BCi = ROL(Eki, 43);
+        Emo ^= Do;
+        BCo = ROL(Emo, 21);
+        Esu ^= Du;
+        BCu = ROL(Esu, 14);
+        Aba =   BCa ^((~BCe)&  BCi );
+        Aba ^= (uint64_t)round_constants[round+1];
+        Abe =   BCe ^((~BCi)&  BCo );
+        Abi =   BCi ^((~BCo)&  BCu );
+        Abo =   BCo ^((~BCu)&  BCa );
+        Abu =   BCu ^((~BCa)&  BCe );
+
+        Ebo ^= Do;
+        BCa = ROL(Ebo, 28);
+        Egu ^= Du;
+        BCe = ROL(Egu, 20);
+        Eka ^= Da;
+        BCi = ROL(Eka, 3);
+        Eme ^= De;
+        BCo = ROL(Eme, 45);
+        Esi ^= Di;
+        BCu = ROL(Esi, 61);
+        Aga =   BCa ^((~BCe)&  BCi );
+        Age =   BCe ^((~BCi)&  BCo );
+        Agi =   BCi ^((~BCo)&  BCu );
+        Ago =   BCo ^((~BCu)&  BCa );
+        Agu =   BCu ^((~BCa)&  BCe );
+
+        Ebe ^= De;
+        BCa = ROL(Ebe, 1);
+        Egi ^= Di;
+        BCe = ROL(Egi, 6);
+        Eko ^= Do;
+        BCi = ROL(Eko, 25);
+        Emu ^= Du;
+        BCo = ROL(Emu, 8);
+        Esa ^= Da;
+        BCu = ROL(Esa, 18);
+        Aka =   BCa ^((~BCe)&  BCi );
+        Ake =   BCe ^((~BCi)&  BCo );
+        Aki =   BCi ^((~BCo)&  BCu );
+        Ako =   BCo ^((~BCu)&  BCa );
+        Aku =   BCu ^((~BCa)&  BCe );
+
+        Ebu ^= Du;
+        BCa = ROL(Ebu, 27);
+        Ega ^= Da;
+        BCe = ROL(Ega, 36);
+        Eke ^= De;
+        BCi = ROL(Eke, 10);
+        Emi ^= Di;
+        BCo = ROL(Emi, 15);
+        Eso ^= Do;
+        BCu = ROL(Eso, 56);
+        Ama =   BCa ^((~BCe)&  BCi );
+        Ame =   BCe ^((~BCi)&  BCo );
+        Ami =   BCi ^((~BCo)&  BCu );
+        Amo =   BCo ^((~BCu)&  BCa );
+        Amu =   BCu ^((~BCa)&  BCe );
+
+        Ebi ^= Di;
+        BCa = ROL(Ebi, 62);
+        Ego ^= Do;
+        BCe = ROL(Ego, 55);
+        Eku ^= Du;
+        BCi = ROL(Eku, 39);
+        Ema ^= Da;
+        BCo = ROL(Ema, 41);
+        Ese ^= De;
+        BCu = ROL(Ese, 2);
+        Asa =   BCa ^((~BCe)&  BCi );
+        Ase =   BCe ^((~BCi)&  BCo );
+        Asi =   BCi ^((~BCo)&  BCu );
+        Aso =   BCo ^((~BCu)&  BCa );
+        Asu =   BCu ^((~BCa)&  BCe );
     }
 
-    vst1q_u64_x4(currentStateWord, s0);
-    vst1q_u64_x4(currentStateWord + 8, s1);
-    vst1q_u64_x4(currentStateWord + 16, s2);
-    vst1q_u64(currentStateWord + 24, s3);
+    //copyToState(state, A)
+    state[ 0] = Aba;
+    state[ 1] = Abe;
+    state[ 2] = Abi;
+    state[ 3] = Abo;
+    state[ 4] = Abu;
+    state[ 5] = Aga;
+    state[ 6] = Age;
+    state[ 7] = Agi;
+    state[ 8] = Ago;
+    state[ 9] = Agu;
+    state[10] = Aka;
+    state[11] = Ake;
+    state[12] = Aki;
+    state[13] = Ako;
+    state[14] = Aku;
+    state[15] = Ama;
+    state[16] = Ame;
+    state[17] = Ami;
+    state[18] = Amo;
+    state[19] = Amu;
+    state[20] = Asa;
+    state[21] = Ase;
+    state[22] = Asi;
+    state[23] = Aso;
+    state[24] = Asu;
 }
