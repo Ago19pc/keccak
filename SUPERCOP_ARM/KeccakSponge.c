93ca7e821ec0fc17eaffeb1d5160148e1b6dc6fe
fillododi <fillo.dodi@gmail.com>
2024-07-06 16:54:16 +0200

    aggiunto supercop arm

---
+++ SUPERCOP_ARM/KeccakSponge.c
@@ -0,0 +1,96 @@
+/*
+Implementation by the Keccak, Keyak and Ketje Teams, namely, Guido Bertoni,
+Joan Daemen, Michaël Peeters, Gilles Van Assche and Ronny Van Keer, hereby
+denoted as "the implementer".
+
+For more information, feedback or questions, please refer to our websites:
+http://keccak.noekeon.org/
+http://keyak.noekeon.org/
+http://ketje.noekeon.org/
+
+To the extent possible under law, the implementer has waived all copyright
+and related or neighboring rights to the source code in this file.
+http://creativecommons.org/publicdomain/zero/1.0/
+*/
+
+#include <stdlib.h>
+#include "KeccakP-1600-SnP.h"
+
+#ifdef ALIGN
+#undef ALIGN
+#endif
+
+#if defined(__GNUC__)
+#define ALIGN(x) __attribute__((aligned(x)))
+#elif defined(_MSC_VER)
+#define ALIGN(x) __declspec(align(x))
+#elif defined(__ARMCC_VERSION)
+#define ALIGN(x) __align(x)
+#else
+#define ALIGN(x)
+#endif
+
+int Sponge(unsigned int rate, unsigned int capacity, const unsigned char *input, size_t inputByteLen, unsigned char suffix, unsigned char *output, size_t outputByteLen)
+{
+    ALIGN(64) unsigned char state[200];
+    unsigned int partialBlock;
+    const unsigned char *curInput = input;
+    unsigned char *curOutput = output;
+    unsigned int rateInBytes = rate/8;
+
+    if (rate+capacity != 1600)
+        return 1;
+    if ((rate <= 0) || (rate > 1600) || ((rate % 8) != 0))
+        return 1;
+    if (suffix == 0)
+        return 1;
+
+    /* Initialize the state */
+    KeccakP1600_StaticInitialize();
+    KeccakP1600_Initialize(state);
+
+    /* First, absorb whole blocks */
+#ifdef SnP_FastLoop_Absorb
+    if (((rateInBytes % (SnP_width/200)) == 0) && (inputByteLen >= rateInBytes)) {
+        /* fast lane: whole lane rate */
+        size_t j;
+        j = SnP_FastLoop_Absorb(state, rateInBytes/(SnP_width/200), curInput, inputByteLen);
+        curInput += j;
+        inputByteLen -= j;
+    }
+#endif
+    while(inputByteLen >= (size_t)rateInBytes) {
+        KeccakP1600_AddBytes(state, curInput, 0, rateInBytes);
+        KeccakP1600_Permute_24rounds(state);
+        curInput += rateInBytes;
+        inputByteLen -= rateInBytes;
+    }
+
+    /* Then, absorb what remains */
+    partialBlock = (unsigned int)inputByteLen;
+    KeccakP1600_AddBytes(state, curInput, 0, partialBlock);
+
+    /* Finally, absorb the suffix */
+    /* Last few bits, whose delimiter coincides with first bit of padding */
+    KeccakP1600_AddByte(state, suffix, partialBlock);
+    /* If the first bit of padding is at position rate-1, we need a whole new block for the second bit of padding */
+    if ((suffix >= 0x80) && (partialBlock == (rateInBytes-1)))
+        KeccakP1600_Permute_24rounds(state);
+    /* Second bit of padding */
+    KeccakP1600_AddByte(state, 0x80, rateInBytes-1);
+    KeccakP1600_Permute_24rounds(state);
+
+    /* First, output whole blocks */
+    while(outputByteLen > (size_t)rateInBytes) {
+        KeccakP1600_ExtractBytes(state, curOutput, 0, rateInBytes);
+        KeccakP1600_Permute_24rounds(state);
+        curOutput += rateInBytes;
+        outputByteLen -= rateInBytes;
+    }
+
+    /* Finally, output what remains */
+    partialBlock = (unsigned int)outputByteLen;
+    KeccakP1600_ExtractBytes(state, curOutput, 0, partialBlock);
+
+    return 0;
+}
---
+++ SUPERCOP_ARM/SHA3.c
@@ -0,0 +1,36 @@
+/*
+Implementation by the Keccak, Keyak and Ketje Teams, namely, Guido Bertoni,
+Joan Daemen, Michaël Peeters, Gilles Van Assche and Ronny Van Keer, hereby
+denoted as "the implementer".
+
+For more information, feedback or questions, please refer to our websites:
+http://keccak.noekeon.org/
+http://keyak.noekeon.org/
+http://ketje.noekeon.org/
+
+To the extent possible under law, the implementer has waived all copyright
+and related or neighboring rights to the source code in this file.
+http://creativecommons.org/publicdomain/zero/1.0/
+*/
+#include "KeccakSponge.c"
+
+int crypto_hash512( unsigned char *out, const unsigned char *in, unsigned long long inlen )
+{
+    return Sponge(576, 1024, in, inlen, 0x06, out, 64);
+}
+int crypto_hash384( unsigned char *out, const unsigned char *in, unsigned long long inlen )
+{
+    return Sponge(832, 768, in, inlen, 0x06, out, 48);
+}
+int crypto_hash256( unsigned char *out, const unsigned char *in, unsigned long long inlen )
+{
+    return Sponge(1088, 512, in, inlen, 0x06, out, 32);
+}
+int crypto_hash_shake256( unsigned char *out, const unsigned char *in, unsigned long long inlen)
+{
+    return Sponge(1088, 512, in, inlen, 0x1F, out, 136);
+}
+int crypto_hash_shake128( unsigned char *out, const unsigned char *in, unsigned long long inlen)
+{
+    return Sponge(1344, 256, in, inlen, 0x1F, out, 168);
+}
